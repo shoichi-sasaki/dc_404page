08d76a4d549cad68ce75560d7a6ea95886572c33
shoichi-sasaki <shoichi-sasaki@x-power.co.jp>
2020-10-27 11:20:44 +0900

    ci test

--- 404page/src/App.vue
+++ 404page/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="It'll be 404 page. please wait..."/>
   </div>
 </template>
 

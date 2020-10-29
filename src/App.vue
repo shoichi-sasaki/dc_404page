9ff8df2ce8fc63d28c17ff457eba8b0bbde64fa7
shoichi-sasaki <shoichi-sasaki@x-power.co.jp>
2020-10-29 10:35:24 +0900

    styleの適用

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <Header/>
-    <img alt="404 image character." src="./assets/Scarecrow.png">
-    I have bad news for you
-    <br/>
-    The page you are looking for might be removed or is temporarily unavailable
-
-    <button>BACK TO HOMEPAGE</button>
+    <div class="f-container">
+      <div class="img-box">
+        <img alt="404 image character." src="./assets/Scarecrow.png">
+      </div>
+      <div class="text-box">
+        <div class="title">
+          I have bad news for you
+        </div>
+        <div class="detail">
+          The page you are looking for might be removed or is temporarily unavailable
+        </div>
+        <button>BACK TO HOMEPAGE</button>
+      </div>
+    </div>
     <Footer/>
   </div>
 </template>
@@ -26,6 +34,15 @@ export default {
 </script>
 
 <style>
+.f-container{
+  display:flex;
+}
+.img-box{
+  flex-basis:43%;
+}
+.text-box{
+  flex-basis:57%;
+}
 img {
   position: absolute;
   left: 5.35%;
@@ -35,12 +52,65 @@ img {
   max-width:539.22px;
   max-height:447.43px;
 }
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+.title{
+  position: absolute;
+  width: 586px;
+  height: 190px;
+  left: 735px;
+  top: 265px;
+
+  font-family: Space Mono;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 95px;
+  letter-spacing: -0.035em;
+
+  color: #333333;
+}
+
+.detail{
+  position: absolute;
+  width: 381px;
+  height: 108px;
+  left: 736px;
+  top: 491px;
+
+  font-family: Space Mono;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 36px;
+  letter-spacing: -0.035em;
+
+  color: #4F4F4F;
+}
+button{
+  position: absolute;
+  width: 216px;
+  height: 68px;
+  left: 740px;
+  top: 663px;
+
+  background: #333333;
+
+  /* text style */
+  /* position: absolute; */
+  /* width: 130px;
+  height: 21px;
+  left: 783px;
+  top: 687px; */
+
+  font-family: Space Mono;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 21px;
+  /* identical to box height */
+
+  letter-spacing: -0.035em;
+  text-transform: uppercase;
+
+  color: #FFFFFF;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,3 +3,10 @@
         Your name @ DevChallenges.io
     </div>
 </template>
+
+<style scoped>
+div{
+    position: absolute;/*←絶対位置*/
+    bottom: 0; /*下に固定*/
+}
+</style>
